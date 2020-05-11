6151abff055992bfc58423d816fc94e88c2ca2e8
xRoier <58123692+xRoier@users.noreply.github.com>
2020-05-12 01:32:23 +0200

    Add files via upload

---
+++ EventHandlers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using EXILED;
+using MEC;
+using DiscordIntegration_Plugin;
+
+
+namespace DiscordAutoBanMsg
+{
+    public class EventHandlers
+    {
+        public Plugin plugin;
+		public string reason;
+		public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+		public void OnPlayerBan(PlayerBannedEvent ev)
+		{
+			if (plugin.replacelowbars)
+			{
+				reason = ev.Details.Reason.Replace("_"," ");
+			}
+			else
+			{
+				reason = ev.Details.Reason;
+			}
+			DateTime ExpireDate = new DateTime(ev.Details.Expires).AddHours(2);
+			string FormattedDate = ExpireDate.ToString("dd/MM/yyyy HH:mm");
+			Timing.CallDelayed(0.5f, () =>
+			{
+				if (plugin.banchannel == 000000000100)
+				{
+					Log.Info($"{plugin.msgnochanneldef}");
+					return;
+				}
+					if (plugin.decoremsg)
+					{
+						ProcessSTT.SendData($"\n> ~~-------------------------------------------~~" +
+$"\n> {plugin.msgbanneduser}: {ev.Details.OriginalName} " +
+$"\n> SteamID64: {ev.Details.Id} " +
+$"\n> {plugin.msgbanissuer}: {ev.Details.Issuer} " +
+$"\n> {plugin.msgreason}: {reason} " +
+$"\n> {plugin.msgexpires}: {FormattedDate} " +
+$"\n> ~~-------------------------------------------~~", plugin.banchannel);
+					} else
+					{
+						ProcessSTT.SendData($"\n{plugin.msgbanneduser}: {ev.Details.OriginalName} " +
+$"\nSteamID64: {ev.Details.Id} " +
+$"\n{plugin.msgbanissuer}: {ev.Details.Issuer} " +
+$"\n{plugin.msgreason}: {reason} " +
+$"\n{plugin.msgexpires}: {FormattedDate} ", plugin.banchannel);
+					}
+			});
+			return;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,163 @@
+﻿using EXILED;
+using MEC;
+
+namespace DiscordAutoBanMsg
+{
+    public class Plugin : EXILED.Plugin
+    {
+        private EventHandlers EventHandlers;
+
+        public override string getName => "DiscordAutoBanMsg";
+        bool isEnabled;
+        public ulong banchannel;
+        public string language;
+        public bool decoremsg;
+        public bool replacelowbars;
+        public string msgconfigdis;
+        public string msgconfigenabled;
+        public string msgnodiscordint;
+        public string msgnochanneldef;
+        public string msgbanneduser;
+        public string msgbanissuer;
+        public string msgreason;
+        public string msgexpires;
+        public string langname;
+        public override void OnEnable()
+        {
+            isEnabled = Config.GetBool("dabm_enable", true);
+            language = Config.GetString("dabm_language");
+            LoadTranslations();
+            Timing.CallDelayed(1f, () => 
+            {
+                if (!isEnabled)
+                {
+                    Log.Info($"{msgconfigdis}");
+                    return;
+                }
+                LoadConfig();
+                Log.Info($"{msgconfigenabled}");
+                EventHandlers = new EventHandlers(this);
+                Events.PlayerBannedEvent += EventHandlers.OnPlayerBan;
+            });
+        }
+        
+
+        public override void OnDisable()
+        {
+            Events.PlayerBannedEvent -= EventHandlers.OnPlayerBan;
+            EventHandlers = null;
+        }
+
+        public void LoadConfig()
+        {
+            isEnabled = Config.GetBool("dabm_enable", true);
+            banchannel = Config.GetULong("dabm_banchannel", 000000000100);
+            language = Config.GetString("dabm_language");
+            decoremsg = Config.GetBool("dabm_decoremsg", true);
+            replacelowbars = Config.GetBool("dabm_reason_replacelowbars", false);
+        }
+
+        public override void OnReload()
+        {
+            LoadConfig();
+        }
+
+        public void LoadTranslations()
+        {
+            switch (language)
+            {
+                case "en":
+                    {
+                        langname = "English";
+                        msgconfigdis = "Plugin is disabled by config.";
+                        msgconfigenabled = "Plugin has been loaded. Language: " + langname;
+                        msgnodiscordint = "DiscordIntegration plugin not found.";
+                        msgnochanneldef = "The ban channel is undefined.";
+                        msgbanissuer = "Banned by";
+                        msgbanneduser = "User";
+                        msgreason = "Reason";
+                        msgexpires = "Expires";
+                        break;
+                    }
+                case "es":
+                    {
+                        langname = "Español";
+                        msgconfigdis = "El plugin está desactivado en la configuración.";
+                        msgconfigenabled = "El plugin ha cargado. Lenguaje: " + langname;
+                        msgnodiscordint = "No se ha encontrado el plugin DiscordIntegration.";
+                        msgnochanneldef = "El canal de baneos no está definido.";
+                        msgbanissuer = "Baneado por";
+                        msgbanneduser = "Usuario";
+                        msgreason = "Razón";
+                        msgexpires = "Expira";
+                        break;
+                    }
+                case "fr":
+                    {
+                        langname = "Français";
+                        msgconfigdis = "Le plugin est désactivé dans les paramètres.";
+                        msgconfigenabled = "Le plugin a été chargé. Langue: " + langname;
+                        msgnodiscordint = "Plugin DiscordIntegration introuvable.";
+                        msgnochanneldef = "Le canal d'interdiction n'est pas défini.";
+                        msgbanissuer = "Interdit par";
+                        msgbanneduser = "Utilisateur";
+                        msgreason = "Raison";
+                        msgexpires = "Expire";
+                        break;
+                    }
+                case "ja":
+                    {
+                        langname = "Japanese";
+                        msgconfigdis = "Plugin is disabled by config.";
+                        msgconfigenabled = "Plugin has been loaded. Language: " + langname;
+                        msgnodiscordint = "DiscordIntegration plugin not found.";
+                        msgnochanneldef = "The ban channel is undefined.";
+                        msgbanissuer = "禁止者";
+                        msgbanneduser = "ユーザー";
+                        msgreason = "理由";
+                        msgexpires = "期限";
+                        break;
+                    }
+                case "ch":
+                    {
+                        langname = "Chinese";
+                        msgconfigdis = "Plugin is disabled by config.";
+                        msgconfigenabled = "Plugin has been loaded. Language: " + langname;
+                        msgnodiscordint = "DiscordIntegration plugin not found.";
+                        msgnochanneldef = "The ban channel is undefined.";
+                        msgbanissuer = "被禁止由";
+                        msgbanneduser = "用户名";
+                        msgreason = "原因";
+                        msgexpires = "过期";
+                        break;
+                    }
+                case "ru":
+                    {
+                        langname = "Russian";
+                        msgconfigdis = "Plugin is disabled by config.";
+                        msgconfigenabled = "Plugin has been loaded. Language: " + langname;
+                        msgnodiscordint = "DiscordIntegration plugin not found.";
+                        msgnochanneldef = "The ban channel is undefined.";
+                        msgbanissuer = "Запрещенныйпо";
+                        msgbanneduser = "пользователь";
+                        msgreason = "причина";
+                        msgexpires = "истекает";
+                        break;
+                    }
+                default:
+                    {
+                        langname = "English (Default)";
+                        msgconfigdis = "Plugin is disabled by config. Language is not defined.";
+                        msgconfigenabled = "Plugin has been loaded. Language: " + langname;
+                        msgnodiscordint = "DiscordIntegration plugin not found.";
+                        msgnochanneldef = "The ban channel is undefined.";
+                        msgbanissuer = "Banned by";
+                        msgbanneduser = "User";
+                        msgreason = "Reason";
+                        msgexpires = "Expires";
+                        break;
+                    }
+            }
+        }
+    }
+}
